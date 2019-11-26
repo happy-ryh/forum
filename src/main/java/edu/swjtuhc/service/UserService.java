feaedae100ffb1e3e836fed1b08f53a7cfe18f60
Asus <Asus@LAPTOP-GC63326B>
2019-11-26 16:26:44 +0800

    userlogin

--- src/main/java/edu/swjtuhc/service/UserService.java
+++ src/main/java/edu/swjtuhc/service/UserService.java
@@ -7,4 +7,5 @@ import edu.swjtuhc.model.SysUser;
 public interface UserService {
 	
 	public List<SysUser> grtAllUser();
+	public String LoginUser(String  username,String password);
 }
--- src/main/java/edu/swjtuhc/servicelmpl/UserServicelmpl.java
+++ src/main/java/edu/swjtuhc/servicelmpl/UserServicelmpl.java
@@ -18,5 +18,23 @@ public class UserServicelmpl implements UserService{
 		// TODO Auto-generated method stub
 		return UserMapper.selectAllUsers();
 	}
-
+	public String LoginUser(String username,String password) {
+		
+		SysUser user = new SysUser();
+		
+		if (username!=null&&password!=null&&username!=""&&password!="") {
+			if (username.equals(user.getUsername())&&password.equals(user.getPassword())) {
+				System.out.println("登录成功！");
+			}else if (username.equals(user.getUsername())&&!password.equals(user.getPassword())) {
+				System.out.println("密码错误！");
+			}else {
+				System.out.println("用户名不存在！");
+			}
+				
+		}else {
+			System.out.println("用户名或密码不能空");
+		}
+		
+		return null;
+	} 
 }
